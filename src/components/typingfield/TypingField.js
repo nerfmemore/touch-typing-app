ee0a533481125e6d79d389da17e627bc905fc736
nerfmemore <gromov.yegor.b@gmail.com>
2022-09-17 01:33:17 +0300

    adding keyPress listener, Accuracy calculator and small changes

--- src/components/typingfield/TypingField.js
+++ src/components/typingfield/TypingField.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import s from './TypingField.module.scss';
+import s from './TypingField.scss';
+
 
 export function TypingField(){
-    const [allKeydown, setKeydown] = useState(0);
-    const [successfullKeydown, setSuccess] = useState(0);
-    const [parsedText, setText] = useState('');
+    const [parsedText, setText] = useState([]);
+    const [mountedDate, setDate] = useState(new Date());
+    const [allKeydowns, setKeydown] = useState(0);
+    const [successfullKeydowns, setSuccess] = useState(0);
 
     useEffect(() => {
         const ParsingText = async() => {
@@ -19,16 +21,56 @@ export function TypingField(){
         ParsingText();        
     }, [])
 
+    useEffect(() => {
+
+    })
+
+    useEffect(() => {
+        function typing(event){
+            console.log(event);
+                setKeydown(allKeydowns + 1);
+                if(event.key === parsedText[successfullKeydowns]){
+                    setSuccess(successfullKeydowns + 1);
+                }
+        }
+        window.addEventListener('keypress', typing);
+        return () => window.removeEventListener('keypress', typing);
+    }, [allKeydowns, successfullKeydowns])
+
     return (
-        <section className={s.main}>
-            <div className={s.container}>
-                <div className={s.text}>
-                    
+        <section className='main'>
+            <div className='container'>
+                <div className='text'>
+                    <LetterList parsedText = {parsedText} successfullKeydowns = {successfullKeydowns}/>
                 </div>
             </div>
-            <div className={s.empty}></div>
+            <div className='empty'></div>
         </section>
     )
 
 }
 
+
+function LetterList(props){
+    const activeStatus = props.successfullKeydowns;
+    const letters = props.parsedText;
+    const listLetters = letters.map((letter, index) => {
+        if (index < activeStatus){
+            return <span className='kpressed' key={index}>{letter}</span>
+        } else if (index == activeStatus){
+            return <span className='kactive' key={index}>{letter}</span>
+        } else if (index > activeStatus){
+            return <span className='kwaiting' key={index}>{letter}</span>
+        }}
+    )
+    return listLetters;
+}
+
+function Accuracy(props){
+    const successes = props.successfullKeydowns;
+    const keyPresses = props.allKeydowns;
+    let calculatedAccuracy = successes / keyPresses * 100;
+
+    return <div className="static">{calculatedAccuracy}%</div>
+}
+//                <Accuracy successfullKeydowns = {successfullKeydowns} allKeydowns = {allKeydowns}/>
